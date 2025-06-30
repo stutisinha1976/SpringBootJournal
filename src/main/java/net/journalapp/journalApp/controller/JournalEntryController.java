fe57b2fc2ab1c95b6b305a87df33f436bc918dc4
Stuti Sinha <stutisinha712@gmail.com>
2025-06-30 20:15:51 +0530

    healthchecker

--- src/main/java/net/journalapp/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/journalapp/journalApp/controller/JournalEntryController.java
@@ -1,4 +1,26 @@
 package net.journalapp.journalApp.controller;
 
+import net.journalapp.journalApp.Entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/journal")
+@RestController
 public class JournalEntryController {
+    private Map<Long,JournalEntry> journalEntries=new HashMap<>();
+    @GetMapping
+    public List<JournalEntry>getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
 }
